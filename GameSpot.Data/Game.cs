f66af16c92b5a6d2c776e9f119bfe1b1bf670874
William casteel <will81656@gmail.com>
2021-03-04 10:20:20 -0600

    Classes

--- GameSpot.Data/Game.cs
+++ GameSpot.Data/Game.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GameSpot.Data
 {
+    public enum MaturityRating { EC = 1, E, T, M, RP, AO }
+    public enum Genre { Sandbox = 1, RTS, FPS, MOBA, RPG, Sports, Puzzle, Party, Adventure, Survival, Horror, Platformer }
     public class Game
     {
+        [Key]
+        public int GameId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int YearOfRelease { get; set; }
+        [Required]
+        public string GameDescription { get; set; }
+
     }
 }
--- GameSpot.Data/GameConsole.cs
+++ GameSpot.Data/GameConsole.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GameSpot.Data
 {
-   public class GameConsole
+    public class GameConsole
     {
+        [Key]
+        public int GameConsoleId { get; set; }
+        public string Console { get; set; }
+        public string Manufacturer { get; set; }
+       
     }
 }
